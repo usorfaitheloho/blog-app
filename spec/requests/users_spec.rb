94ba310bf6de58c481034a0bf0c5131b0c263e5b
usorfaitheloho <usorfaitheloho@gmail.com>
2022-06-10 02:10:19 -0700

    Fix linters

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,46 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-    context 'GET /index' do
-        it 'should return the correct response' do
-            get '/users'
-            expect(response).to have_http_status(200)
-        end
+  context 'GET /index' do
+    it 'should return the correct response' do
+      get '/users'
+      expect(response).to have_http_status(200)
     end
+  end
 
-    context 'GET /index' do
-        it 'should return the correct body' do
-            get '/users'
-             expect(response.body).to include('Welcome to the blog')
-        end
+  context 'GET /index' do
+    it 'should return the correct body' do
+      get '/users'
+      expect(response.body).to include('Welcome to the blog')
     end
-    
-    context 'GET /index' do
-        it 'should return the correct template' do
-            get '/users'
-            expect(response).to render_template(:index)
-        end
-    end
+  end
 
-    context 'GET /show' do
-        it 'should return the correct response' do
-            get '/users/show'
-            expect(response).to have_http_status(200)
-        end
+  context 'GET /index' do
+    it 'should return the correct template' do
+      get '/users'
+      expect(response).to render_template(:index)
     end
+  end
 
-    context 'GET /show' do
-        it 'should return the correct body' do
-            get '/users/show'
-             expect(response.body).to include('User profile')
-        end
+  context 'GET /show' do
+    it 'should return the correct response' do
+      get '/users/show'
+      expect(response).to have_http_status(200)
     end
+  end
 
-    context 'GET /show' do
-        it 'should return the correct template' do
-            get '/users/show'
-            expect(response).to render_template(:show)
-        end
+  context 'GET /show' do
+    it 'should return the correct body' do
+      get '/users/show'
+      expect(response.body).to include('User profile')
     end
-    
+  end
+
+  context 'GET /show' do
+    it 'should return the correct template' do
+      get '/users/show'
+      expect(response).to render_template(:show)
+    end
+  end
 end
