4850ab7d099c220f16a69fbb6177cc29a75796a7
usorfaitheloho <usorfaitheloho@gmail.com>
2022-06-15 10:22:33 -0700

    Add current_user method and refactor code

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :comments
+  after_save :update_posts_counter
 
   private
 
-  def update_post_counter
-    user.increment(:posts_counter)
+  def update_posts_counter
+    user.increment!(:posts_counter)
   end
 
   def recent_comment
