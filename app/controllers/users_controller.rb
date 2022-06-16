cbaff0b3852d80492cee8c84a29843252e5a95e4
usorfaitheloho <usorfaitheloho@gmail.com>
2022-06-16 11:50:44 -0700

    Add post index template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.recent_posts
   end
+
+  def user_params
+    params.require(:user).permit(:Name, :Bio, :Photo, :PostsCounter)
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, counter_cache: :comments_counter
   belongs_to :post
   after_save :update_comments_counter
 
   def update_comments_counter
-    post.increment!(:comments_counter)
+    post.update(comments_count: post.comments.size)
   end
 end
