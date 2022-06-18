3bf84f1e14f9bbeb076c5c402b78f55d8a6e76e3
usorfaitheloho <usorfaitheloho@gmail.com>
2022-06-18 16:09:53 -0700

    Add test for post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  first_user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 2)
+  subject { Post.new(title: 'Hello', text: 'This is my first post', likes_counter: 3, comments_counter: 4) } 
+ 
+  before {subject.save }
+ 
+  it "title should be present" do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+ 
+  it "title should not 250" do
+    subject.title = 'm' * 400 
+    expect(subject).to_not be_valid
+  end
+ 
+   it "text should be present" do
+    subject.text = nil
+    expect(subject).to_not be_valid
+  end
+ 
+  it "comments counter should be greater than 0" do
+    subject.comments_counter = 0
+    expect(subject).to_not be_valid
+   end
+ 
+  it "likes counter should be greater than 0" do
+    subject.likes_counter = 0
+    expect(subject).to_not be_valid
+   end
+ end 
