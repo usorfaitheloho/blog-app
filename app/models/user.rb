4a3e5e6097f0878dc44585a9c87d148e084f37b4
usorfaitheloho <usorfaitheloho@gmail.com>
2022-06-18 15:50:30 -0700

    Validate post counter for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :posts_counter, numericity: {only_integer: true, :greater_than_or_equal_to: 0}
   
   def recent_posts
     posts.order(created_at: :desc).limit(3)
