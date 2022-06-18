ab75e6fbf71c2ee052834f8bda068456a5c3683f
usorfaitheloho <usorfaitheloho@gmail.com>
2022-06-17 19:39:54 -0700

    Add new method for Post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,11 @@ class PostsController < ApplicationController
   def show
     @post = Post.includes(:user, comments: [:user]).find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+  end
 end
