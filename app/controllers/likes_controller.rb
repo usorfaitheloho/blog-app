9b0aaa66598eb156b197c6b19b6995ab1ffc7e81
usorfaitheloho <usorfaitheloho@gmail.com>
2022-06-18 03:41:48 -0700

    Create likes for a post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,4 +2,22 @@ class LikesController < ApplicationController
     def new
         @likes = Likes.new
       end
+
+      def create
+        @post = Post.includes(:user, comments: [:user]).find(:id)
+        @post_author = @post.user
+    
+        @user = current_user
+    
+        @liked_posts_by_user = Like.where(user: @user, post: @post)
+    
+        destroy && return if @liked_posts_by_user.present?
+    
+        @like = Like.create(user: @user, post: @post)
+    
+        return unless @like.save
+    
+        flash[:success] = 'You have liked this post'
+        redirect_to @like
+      end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def index
     user_id = params[:user_id]
     @user = User.find(user_id)
-    @posts = Post.includes(:user).where(user: params[:user_id]).references(:user)
+    @posts = @user.posts
   end
 
   def show
