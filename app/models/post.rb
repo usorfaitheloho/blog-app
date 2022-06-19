25869a88d63dd3b6e04e6662fa7de7f3b37b6f55
usorfaitheloho <usorfaitheloho@gmail.com>
2022-06-18 17:17:31 -0700

    Fix linter error

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,10 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   after_save :update_posts_counter
 
-  validates :title, presence: true, length: {maximum: 250}
+  validates :title, presence: true, length: { maximum: 250 }
   validates :text, presence: true
-  validates :comments_counter, numericality: {only_integer: true, greater_than_or_equal_to : 0}
-  validates :likes_counter, numericality: {only_integer: true, greater_than_or_equal_to : 0}
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_posts_counter
     user.update(posts_counter: user.posts.size)
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,54 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:author) {User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 2)}
+  let(:author) { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 2) }
 
-  subject { Post.new(user: author, title: 'Hello', text: 'This is my first post', likes_counter: 3, comments_counter: 4) }
- 
-  before {subject.save }
- 
-  it "title should be present" do
+  subject do
+    Post.new(user: author, title: 'Hello', text: 'This is my first post', likes_counter: 3, comments_counter: 4)
+  end
+
+  before { subject.save }
+
+  it 'title should be present' do
     subject.title = nil
     expect(subject).to_not be_valid
   end
- 
-  it "title should not 250" do
-    subject.title = 'm' * 400 
+
+  it 'title should not 250' do
+    subject.title = 'm' * 400
     expect(subject).to_not be_valid
   end
- 
-   it "text should be present" do
+
+  it 'text should be present' do
     subject.text = nil
     expect(subject).to_not be_valid
   end
- 
-  it "comments counter should not be less than 0" do
+
+  it 'comments counter should not be less than 0' do
     subject.comments_counter = -1
     expect(subject).to_not be_valid
-   end
- 
-   it "likes counter should not be less than 0" do
-     subject.likes_counter = -1
-    expect(subject).to_not be_valid
-   end
+  end
 
-   it "comment counter should be greater than or equal to 0" do
+  it 'likes counter should not be less than 0' do
+    subject.likes_counter = -1
+    expect(subject).to_not be_valid
+  end
+
+  it 'comment counter should be greater than or equal to 0' do
     subject.comments_counter = 1
     expect(subject).to be_valid
   end
- 
-    it "likes counter should be greater than or equal to 0" do
+
+  it 'likes counter should be greater than or equal to 0' do
     subject.likes_counter = 1
     expect(subject).to be_valid
   end
- 
-   it "comment counter should be an integer" do
+
+  it 'comment counter should be an integer' do
     subject.comments_counter = 1.2
     expect(subject).to_not be_valid
   end
- 
-   it "likes counter should be an integer" do
+
+  it 'likes counter should be an integer' do
     subject.likes_counter = 1.2
     expect(subject).to_not be_valid
   end
- end 
+end
