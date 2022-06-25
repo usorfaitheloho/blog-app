5617144f29baa2174517650f2c7a029c554be0cc
usorfaitheloho <usorfaitheloho@gmail.com>
2022-06-25 09:18:55 -0700

    install bundle

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :email, :uniqueness => {:allow_blank => true}
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
